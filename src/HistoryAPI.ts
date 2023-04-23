fff43fa288d84431339e4ce8b481dbd435f3face
Teppo Kurki <teppo.kurki@iki.fi>
2023-04-23 23:10:53 +0300

    wip: function getValues

--- src/HistoryAPI.ts
+++ src/HistoryAPI.ts
@@ -1,52 +1,30 @@
-import { ZonedDateTime } from "@js-joda/core";
+import { ZonedDateTime } from '@js-joda/core'
 
-import {
-  Request,
-  Response,
-  Router,
-} from "express";
-import { SKInflux } from "./influx";
-
+import { Request, Response, Router } from 'express'
+import { SKInflux } from './influx'
+import { FluxResultObserver, FluxTableMetaData } from '@influxdata/influxdb-client'
 
 export function registerHistoryApiRoute(
   router: Pick<Router, 'get'>,
   influx: SKInflux,
   selfId: string,
-  debug: (k: string) => void
+  debug: (k: string) => void,
 ) {
-
-  router.get(
-    "/signalk/v1/history/values",
-    (req: Request, res: Response) => {
-      const {from, to, context} = getFromToContext(req as FromToContextRequest, selfId)
-      res.json(getValues(influx, context, from, to, debug, req))
-    }
-  );
+  router.get('/signalk/v1/history/values', (req: Request, res: Response) => {
+    const { from, to, context } = getFromToContext(req as FromToContextRequest, selfId)
+    getValues(influx, context, from, to, debug, req, res)
+  })
   // router.get(
   //   "/signalk/v1/history/contexts",
-  //   asyncHandler(
-  //     fromToHandler(
-  //       (...args) => getContexts.apply(this, [influx, ...args]),
-  //       contextsDebug
-  //     )
-  //   )
-  // );
   // router.get(
   //   "/signalk/v1/history/paths",
-  //   asyncHandler(
-  //     fromToHandler(
-  //       (...args) => getPaths.apply(this, [influx, selfId, ...args]),
-  //       pathsDebug
-  //     )
-  //   )
-  // );
 }
 
 async function getContexts(
   influx: SKInflux,
   from: ZonedDateTime,
   to: ZonedDateTime,
-  debug: (s: string) => void
+  debug: (s: string) => void,
 ): Promise<string[]> {
   return Promise.resolve(['self'])
   // return influx
@@ -61,8 +39,8 @@ async function getPaths(
   from: ZonedDateTime,
   to: ZonedDateTime,
   debug: (s: string) => void,
-  req: Request
-) :Promise<string[]> {
+  req: Request,
+): Promise<string[]> {
   // const query = `SHOW MEASUREMENTS`;
   // console.log(query);
   // return influx.then((i) => i.query(query)).then((d) => d.map((r:any) => r.name));
@@ -70,20 +48,20 @@ async function getPaths(
 }
 
 interface ValuesResult {
-  context: string;
+  context: string
   range: {
-    from: string;
-    to: string;
-  };
+    from: string
+    to: string
+  }
   values: {
-    path: string;
-    method: string;
-    source?: string;
-  }[];
-  data: ValuesResultRow[];
+    path: string
+    method: string
+    source?: string
+  }[]
+  data: ValuesResultRow[]
 }
 
-type ValuesResultRow = any[];
+type ValuesResultRow = any[]
 
 async function getValues(
   influx: SKInflux,
@@ -91,126 +69,118 @@ async function getValues(
   from: ZonedDateTime,
   to: ZonedDateTime,
   debug: (s: string) => void,
-  req: Request
+  req: Request,
+  res: Response,
 ): Promise<ValuesResult | void> {
   const timeResolutionSeconds = req.query.resolution
     ? Number.parseFloat(req.query.resolution as string)
-    : (to.toEpochSecond() - from.toEpochSecond()) / 500;
+    : (to.toEpochSecond() - from.toEpochSecond()) / 500
 
-  debug(context);
-  const pathExpressions = (req.query.paths as string || "")
-    .replace(/[^0-9a-z\.,\:]/gi, "")
-    .split(",");
-  const pathSpecs: PathSpec[] = pathExpressions.map(splitPathExpression);
-  return undefined
-  // const queries = pathSpecs
-  //   .map(
-  //     ({ aggregateFunction, path }) => `
-  //     SELECT
-  //       ${aggregateFunction} as value
-  //     FROM
-  //     "${path}"
-  //     WHERE
-  //       "context" = '${context}'
-  //       AND
-  //       time > '${from.toString()}' and time <= '${to.toString()}'
-  //     GROUP BY
-  //       time(${Number(timeResolutionSeconds * 1000).toFixed(0)}ms)`
-  //   )
-  //   .map((s) => s.replace(/\n/g, " ").replace(/ +/g, " "));
-  // queries.forEach((s) => debug(s));
+  debug(context)
+  const pathExpressions = ((req.query.paths as string) || '').replace(/[^0-9a-z\.,\:]/gi, '').split(',')
+  const pathSpecs: PathSpec[] = pathExpressions.map(splitPathExpression)
+  const valuesResult: ValuesResult = {
+    context,
+    range: {
+      from: from.toString(),
+      to: to.toString(),
+    },
+    values: pathSpecs.map(({ path, aggregateMethod }: PathSpec) => ({ path, method: aggregateMethod })),
+    data: [],
+  }
+  const o: FluxResultObserver<any> = {
+    next: (row: string[], tableMeta: FluxTableMetaData) => {
+      valuesResult.data.push([
+        {
+          time: tableMeta.get(row, '_time'),
+          value: tableMeta.get(row, '_value'),
+        },
+      ])
+      return true
+    },
+    error: (s: Error) => {
+      res.status(500)
+      res.json(s)
+    },
+    complete: () => res.json(valuesResult),
+  }
 
-  // const x: Promise<IResults<any>[]> = Promise.all(
-  //   queries.map((q: string) => influx.then((i) => i.query(q)))
-  // );
-
-  // return x.then((results: IResults<any>[]) => ({
-  //   context,
-  //   values: pathSpecs.map(({ path, aggregateMethod }) => ({
-  //     path,
-  //     method: aggregateMethod,
-  //     source: null,
-  //   })),
-  //   range: { from: from.toString(), to: to.toString() },
-  //   data: toDataRows(
-  //     results.map((r) => r.groups()),
-  //     pathSpecs.map((ps) => ps.extractValue)
-  //   ),
-  // }));
+  const query = `
+    from(bucket: "${influx.bucket}")
+    |> range(start: ${from.toString()}, stop: ${to.toString()})
+    |> filter(fn: (r) =>
+      r._measurement == "${pathSpecs[0].path}" and
+      r._field == "value"
+    ) 
+    |> aggregateWindow(every: ${timeResolutionSeconds.toFixed(0)}s, fn: ${pathSpecs[0].aggregateFunction})
+    `
+  console.log(query)
+  influx.queryApi.queryRows(query, o)
 }
 
 function getContext(contextFromQuery: string, selfId: string) {
-  if (
-    !contextFromQuery ||
-    contextFromQuery === "vessels.self" ||
-    contextFromQuery === "self"
-  ) {
-    return `vessels.${selfId}`;
+  if (!contextFromQuery || contextFromQuery === 'vessels.self' || contextFromQuery === 'self') {
+    return `vessels.${selfId}`
   }
-  return contextFromQuery.replace(/ /gi, "");
+  return contextFromQuery.replace(/ /gi, '')
 }
 
-
 interface PathSpec {
-  path: string;
-  aggregateMethod: string;
-  aggregateFunction: string;
-  extractValue: (x: any) => any;
+  path: string
+  aggregateMethod: string
+  aggregateFunction: string
+  extractValue: (x: any) => any
 }
 
 interface WithValue {
   value?: any
 }
-type ExtractValue = (r:WithValue) => any
+type ExtractValue = (r: WithValue) => any
 const EXTRACT_POSITION = (r: WithValue) => {
   if (r.value) {
-    const position = JSON.parse(r.value);
-    return [position.longitude, position.latitude];
+    const position = JSON.parse(r.value)
+    return [position.longitude, position.latitude]
   }
-  return null;
-};
-const EXTRACT_NUMBER = (r: WithValue) => Number(r.value);
+  return null
+}
+const EXTRACT_NUMBER = (r: WithValue) => Number(r.value)
 
 function splitPathExpression(pathExpression: string): PathSpec {
-  const parts = pathExpression.split(":");
-  let aggregateMethod = parts[1] || "average";
-  let extractValue: ExtractValue = EXTRACT_NUMBER;
-  if (parts[0] === "navigation.position") {
-    aggregateMethod = "first";
-    extractValue = EXTRACT_POSITION;
+  const parts = pathExpression.split(':')
+  let aggregateMethod = parts[1] || 'average'
+  let extractValue: ExtractValue = EXTRACT_NUMBER
+  if (parts[0] === 'navigation.position') {
+    aggregateMethod = 'first'
+    extractValue = EXTRACT_POSITION
   }
   return {
     path: parts[0],
     aggregateMethod,
     extractValue,
-    aggregateFunction: functionForAggregate[aggregateMethod] as string || "MEAN(value)",
-  };
+    aggregateFunction: (functionForAggregate[aggregateMethod] as string) || 'mean()',
+  }
 }
 
-const functionForAggregate: {[key:string]: string} = {
-  average: "mean()",
-  min: "min()",
-  max: "max()",
-  first: "first()",
-};
+const functionForAggregate: { [key: string]: string } = {
+  average: 'mean',
+  min: 'min',
+  max: 'max',
+  first: 'first',
+}
 
-type FromToHandler<T = any> = (
-  from: ZonedDateTime,
-  to: ZonedDateTime,
-  debug: (d: string) => void,
-  req: Request,
-  res: Response
-) => Promise<T>;
+type FromToContextRequest = Request<
+  unknown,
+  unknown,
+  unknown,
+  {
+    from: string
+    to: string
+    context: string
+  }
+>
 
-
-type FromToContextRequest = Request<unknown, unknown, unknown, {
-  from: string
-  to: string
-  context: string
-}>
-
-const getFromToContext = ( {query}: FromToContextRequest, selfId: string) => {
-  const from = ZonedDateTime.parse(query["from"]);
-  const to = ZonedDateTime.parse(query["to"]);
-  return {from, to, context: getContext(query.context, selfId)}
+const getFromToContext = ({ query }: FromToContextRequest, selfId: string) => {
+  const from = ZonedDateTime.parse(query['from'])
+  const to = ZonedDateTime.parse(query['to'])
+  return { from, to, context: getContext(query.context, selfId) }
 }
